ffa6b229746a5ec9941d0ebd74613b9405841de7
sadiki <sxs6590@rit.edu>
2018-11-15 19:19:38 -0500

    Removed navbar from shared module and fixed urls

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { NavbarComponent } from './navbar/navbar.component';
 import { BaconizePipe } from './pipes/baconize.pipe';
 
 @NgModule({
   declarations: [
     BaconizePipe,
-    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     BaconizePipe,
-    NavbarComponent
   ]
 })
 export class SharedModule { }
