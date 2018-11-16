23f72b5e1038a3e61f6aef57f48112ac68ad6e3f
Sahil Makhijani <svmakhijani94@gmail.com>
2018-11-15 19:12:44 -0500

    Created login method for LoginComponent

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { User } from '../../core/models/User';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  isValid = true;
+  private sessionUser = localStorage.getItem('user');
+  private subscription: Subscription;
+  private user: User;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router, private location: Location) { }
 
   ngOnInit() {
+    if (this.sessionUser) {
+      this.router.navigate(['']);
+    }
   }
 
+  login() {
+    this.userService.login(this.user).subscribe((user) => {
+      if (!user) {
+        this.isValid = false;
+      } else {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.router.navigate(['']);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
