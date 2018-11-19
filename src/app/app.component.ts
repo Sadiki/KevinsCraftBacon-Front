e2c2f110499e8c907f6a22e853e30c0d326c8279
Sahil Makhijani <svmakhijani94@gmail.com>
2018-11-19 12:00:59 -0500

    Added keyboard shortcuts to entire sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,22 @@ export class AppComponent {
   handleKeypress(event: KeyboardEvent) {
     if (event.key == 'Escape') {
       this.sidenav.toggle();
+    } else if (event.altKey) {
+      if (event.key == '0') {
+        document.getElementById('home').click();
+      } else if (event.key == '1') {
+        document.getElementById('profile').click();
+      } else if (event.key == '2') {
+        document.getElementById('login').click();
+      } else if (event.key == '3') {
+        document.getElementById('register').click();
+      } else if (event.key == '4') {
+        document.getElementById('cart').click();
+      } else if (event.key == '5') {
+        document.getElementById('orders').click();
+      } else if (event.key == '6') {
+        document.getElementById('logout').click();
+      }
     }
   }
 }
