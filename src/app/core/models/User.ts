bdb484c2cfdb09ed3bbfcd3d1f6d0ad14ad63840
Sahil Makhijani <svmakhijani94@gmail.com>
2018-11-20 10:16:30 -0500

    Made User interface better

--- src/app/core/models/User.ts
+++ src/app/core/models/User.ts
@@ -1,9 +1,9 @@
 export interface User {
     cust_id?: number;
-    firstName?: string;
-    lastName?: string;
     username?: string;
     password?: string;
+    firstName?: string;
+    lastName?: string;
     email?: string;
     phoneNumber?: string;
     streetAddress?: string;
